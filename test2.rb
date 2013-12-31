f1fcee981602f7320718c9fc1e373e3df9051b2d
lashd <leon.davis@lad-tech.com>
2013-12-31 14:25:37 +0000

    LAD: remove some redundant files and add to the ignore list

--- test2.rb
+++
@@ -1,2 +0,0 @@
-require 'httparty'
-puts HTTParty.get('https://api.sandbox.freeagent.com/v2/company', headers: {'Authorization' => "Bearer 1P2wjf44HunJDKWKXdrLmGUT-C5-EXwz3o2QnYh7d", 'User-Agent' => 'faraday'})
